29d21dbeadac1ffd8a6d81eee5b3c376291caec5
Sean Gray <sean.myles.gray@gmail.com>
2020-12-30 13:54:16 +0000

    added getCity to forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -17,6 +17,10 @@ const getWeather = async (id) => {
 const getCity = async (city) => {
 
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
-  const query = ''
+  const query = `?apikey=${key}&q=${city}`
 
+  const response = await fetch(base + query)
+  const data = await response.json
+
+  return data[0]
 }
